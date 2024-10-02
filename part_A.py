0aba612b3fd53abfb89aad0d6cca871ec749ec77
Daniel Diaz Ramos <daniel.ramos@student.nhh.no>
2024-10-02 15:14:26 +0200

    Finished the two functions: One with loops and one with numpy

--- part_A.py
+++ part_A.py
@@ -20,6 +20,52 @@ def std_loops(x):
     sd : float
         Standard deviation of the list of numbers.
     """
+    
+    # WE CALCULATE THE LENGTH OF X
+
+    count = 0
+
+    while x[count:]:
+        count = count+1
+
+    N = count 
+
+
+    # WE CALCULATE THE SUM OF ALL TERMS AND CALCULATE THE MEAN OF X
+
+    count = N-1
+    sumx = 0
+
+    while count >= 0:
+        sumx = sumx + x[count]
+        count = count-1
+
+    meanx = (1/N) * sumx
+
+
+    # WE COMPUTE THE SUM OF THE SQUARES AND THEN IT'S MEAN (S)
+
+    count = N-1
+    sumx2 = 0
+
+    while count >= 0:
+        sumx2 = sumx2 + ((x[count])**2)
+        count = count-1
+
+    S = (1/N) * sumx2
+
+
+    # WE COMPUTE THE VARIANCE
+
+    var1 = meanx**2
+    variance = S - var1
+
+    # WE COMPUTE THE STANDARD DEVIATION
+
+    sd = variance**(1/2)
+    
+    return sd
+    
 
 def std_builtin(x):
     """
@@ -47,9 +93,11 @@ def std_builtin(x):
 
     # WE COMPUTE THE VARIANCE
 
-    var1 = np.sum(meanx**2)
+    var1 = meanx**2
     variance = S - var1
 
     # WE COMPUTE THE STANDARD DEVIATION
 
     sd = variance**(1/2)
+    
+    return sd
